fb1a29de2c2b8fb83dc6c8f4e1e0a3a3cd7e46d8
tkhwang <tkhwang@gmail.com>
2018-12-15 14:46:12 +0900

    [+] Config env for create-react-app using REACT_APP_*

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,3 +1,6 @@
+const config = require('../config');
+const { SERVER_URL } = config();
+
 export const LOGIN = 'LOGIN';
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
@@ -8,11 +11,8 @@ export const login = (email, password) => {
     type: LOGIN,
     promise: {
       method: 'post',
-      url: 'http://localhost:5000/signin',
-      data: {
-        email,
-        password
-      }
+      url: SERVER_URL + '/signin',
+      data: { email, password }
     }
   };
 };
