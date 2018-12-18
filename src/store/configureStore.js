7bfe2af0b8be029f3d507022a9a8c442d2ca09dc
tkhwang <tkhwang@gmail.com>
2018-12-18 13:46:47 +0900

    Fix minor bugs

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,8 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
 
 import reducer from '../reducers';
-import promiseMiddleware from '../middleware/promiseMiddleware.js';
 
 const logger = createLogger();
 const store = createStore(reducer, applyMiddleware(logger, ReduxThunk));
